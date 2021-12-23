4e7bacadfaab436508cf36475c6d266b74732ccf
unknown <panymari84@gmail.com>
2021-12-23 11:00:56 +0300

    Changes

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,6 @@
 
-import { useRef } from 'react';
 import classes from "./Header.module.scss";
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 
 const Header = () => {
   const [showHeader, setShowHeader] = useState(false);
@@ -10,7 +9,6 @@ const Header = () => {
   const headerRef = useRef();
 
   const changeVisibilityHeader = () => {
-    console.log(headerRef);
     if (window.scrollY >= headerRef.current.clientHeight) {
       setShowHeader(true);
     } else {
@@ -18,39 +16,49 @@ const Header = () => {
     }
   }
 
+  useEffect(() => {
+    window.addEventListener('scroll', ()=>window.requestAnimationFrame(changeVisibilityHeader));
 
-  window.removeEventListener('scroll', changeVisibilityHeader);
-  window.addEventListener('scroll', changeVisibilityHeader);
+    return () => window.removeEventListener('scroll', ()=>window.cancelAnimationFrame(changeVisibilityHeader));
 
+  }, [showHeader]);
+    
+    
   return (
     <div className={showHeader ? classes.headerHide : classes.headerShow} ref={headerRef}>
 
-      <div className="left-part">
-        <div className="logo" />
-        <div className="slogan">Your Learning at IBM</div>
-        <div className="angle-down-block">
-          <div
-            onMouseEnter={() => setShowBlock(true)}
-            onMouseLeave={() => setShowBlock(false)}>
+      <div className={classes.leftPart}>
+        <div className={classes.logo} />
+        <div className={classes.slogan}>Your Learning at IBM</div>
+        <div 
+          className={classes.angleDownBlock}             
+          onMouseEnter={() => setShowBlock(true)}
+          onMouseLeave={() => setShowBlock(false)}>
+          <div>
             Recommendations
+          </div>
+          <div className={showBlock ? classes.blockShow : classes.blockHide}>
+            <div>block 1</div>
+            <div>block 2</div>
+            <div>block 3</div>
           </div>
           <i class="fa fa-angle-down"></i>
         </div>
-        <div className="angle-down-block">
+        <div className={classes.angleDownBlock}>
           <div>Learning</div>
           <i class="fa fa-angle-down"></i>
         </div>
-        <div className="angle-down-block">
+        <div className={classes.angleDownBlock}>
           <div>Team</div>
           <i class="fa fa-angle-down"></i>
         </div>
       </div>
 
-      <div className="right-part">
+      <div className={classes.rightPart}>
         <div><i class="fa fa-search"></i></div>
         <div><i class="fa fa-question-circle"></i></div>
         <div><i class="fa fa-user" aria-hidden="true"></i></div>
-        <div className="ellipsis-icon">
+        <div className={classes.ellipsisIcon}>
           <i class="fa fa-ellipsis-v"></i>
           <i class="fa fa-ellipsis-v"></i>
           <i class="fa fa-ellipsis-v"></i>
